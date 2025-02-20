46c3b61c8a6c905ebb173dbdfef22ef17f9e8028
João Pinto <joao.pinto@premium-minds.com>
2025-02-20 19:08:47 +0000

    add instance security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ locals {
 
 resource "aws_instance" "this" {
   ami           = local.ubuntu_ami
-  instance_type = "t3.micro"
+  instance_type = "t3.nano"
 
   vpc_security_group_ids = []
 
@@ -18,4 +18,41 @@ resource "aws_instance" "this" {
   tags = {
     Name = "sinfo-workshop-16"
   }
+}
+
+resource "aws_security_group" "allow_traffic" {
+  name        = "allow_traffic-ws-sinfo-16"
+  description = "Allow HTTP and SSH inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-03b4b4411526cdd9c"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group_rule" "allow_http_ingress" {
+  type              = "ingress"
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_traffic.id
+}
+
+resource "aws_security_group_rule" "allow_ssh_ingress" {
+  type              = "ingress"
+  from_port         = "22"
+  to_port           = "22"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_traffic.id
+}
+
+resource "aws_security_group_rule" "allow_all_egress" {
+  type              = "egress"
+  from_port         = "0"
+  to_port           = "0"
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_traffic.id
 }
