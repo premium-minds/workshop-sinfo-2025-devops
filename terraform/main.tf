1d1c870e2bcea29e2c4503a2c20ba87ba1e0871f
João Pinto <joao.pinto@premium-minds.com>
2025-02-20 18:54:28 +0000

    Create aws instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,19 @@ locals {
 }
 
 
+resource "aws_instance" "this" {
+  ami           = local.ubuntu_ami
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = []
+
+  user_data_base64 = filebase64("${path.module}/cloud-config.yaml")
+
+  root_block_device {
+    volume_size = "10"
+  }
+
+  tags = {
+    Name = "sinfo-workshop-16"
+  }
+}
